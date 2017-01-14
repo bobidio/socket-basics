85b6a9372d2d7e9aa7d9ec02127c8adc558ae8e6
bobidio <bobidio@gmail.com>
2017-01-14 20:23:03 +0200

    add events

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,9 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Conncted to socket.io server!');
 });
+
+socket.on('message', function (message){
+	console.log('new message: ');
+	console.log(message.text);
+
+});
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var PORT = process.env.PORT || 3000;
-var express= require('express');
+var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function (){
+io.on('connection', function (socket) {
 	console.log('user connected via socket.io');
+
+	socket.on('message', function (message){
+		console.log('message received: ' + message.text);
+		socket.broadcast.emit('message', message);
+
+	});
+
+	socket.emit('message', {
+		text: 'welcome to the chat'
+	});
 });
-http.listen(PORT, function  () {
+http.listen(PORT, function() {
 	console.log('Server started!');
 });
